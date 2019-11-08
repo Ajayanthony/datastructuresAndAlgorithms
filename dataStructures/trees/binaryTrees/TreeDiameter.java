99362eed829ae83f20a84616d2859d77f6d59a7e
Ajay <ajayedanthony@gmail.com>
2019-11-08 08:38:31 +0530

    Algorithm for finding the diameter of binary tree.

---
+++ dataStructures/trees/binaryTrees/TreeDiameter.java
@@ -0,0 +1,23 @@
+package trees.binaryTrees;
+
+/**
+ * Give an algorithm for finding the diameter of the binary tree. The diameter of a
+ * tree (sometimes called the width) is the number of nodes on the longest path between two
+ * leaves in the tree.
+ */
+public class TreeDiameter {
+
+    public static int getTreeDiameter(Node root) {
+        if (root == null)
+            return 0;
+
+        int leftDiameter = getTreeDiameter(root.left);
+        int rightDiameter = getTreeDiameter(root.right);
+
+        int rootDiameter = BinaryTreeUtils.getHeight(root.left) +
+                BinaryTreeUtils.getHeight(root.right) + 3;
+        int childDiameter = Math.max(leftDiameter, rightDiameter);
+
+        return Math.max(rootDiameter, childDiameter);
+    }
+}
