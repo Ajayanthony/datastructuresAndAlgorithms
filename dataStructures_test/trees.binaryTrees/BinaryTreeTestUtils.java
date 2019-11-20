b69202aace05c4a385074d8dc4a76f6c92975d5b
Ajay <ajayedanthony@gmail.com>
2019-11-20 20:44:58 +0530

    Created Test folder for Binary Trees with a util class to generate test binary tree.

---
+++ dataStructures_test/trees.binaryTrees/BinaryTreeTestUtils.java
@@ -0,0 +1,16 @@
+package trees.binaryTrees;
+
+public class BinaryTreeTestUtils {
+
+    public static Node getTestBinaryTree() {
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.right.left = new Node(6);
+        root.right.right = new Node(7);
+
+        return root;
+    }
+}
