6365a86f1eb929f794e3d1a32ed9963b2bcadb2e
Ajay <ajayedanthony@gmail.com>
2019-12-24 19:40:42 +0530

    x to 0

--- dataStructures/string/ConvertXtoO_2D.java
+++ dataStructures/string/ConvertXtoO_2D.java
@@ -3,43 +3,13 @@ package string;
 /**
  * Given a 2D board containing ‘X’ and ‘O’, capture all regions surrounded by ‘X’.
  * A region is captured by flipping all ‘O’s into ‘X’s in that surrounded region .
+ * The elements not surrounded by ‘X’ means that exists a path of
+ * elements ‘O’ to a border.
  */
 public class ConvertXtoO_2D {
 
-    /**
-     * Converts the 2D matrix in place, but affects the input matrix reference.
-     * Similar algo can be applied on a copy matrix to avoid affecting the input reference.
-     *
-     * @param input
-     */
     public static void convertXtoO(char[][] input) {
 
-        for (int i = 1; i < input.length - 1; i++) {
-            for (int j = 1; j < input[0].length - 1; j++) {
-                if (input[i][j] == 'O') {
-                    if (input[i - 1][j - 1] == 'X'
-                            && input[i - 1][j] == 'X'
-                            && input[i - 1][j + 1] == 'X'
-                            && input[i][j - 1] == 'X'
-                            && input[i][j + 1] == 'X'
-                            && input[i + 1][j - 1] == 'X'
-                            && input[i + 1][j] == 'X'
-                            && input[i + 1][j + 1] == 'X'
-                    ) {
-                        input[i][j] = 'Y';
-                    }
-                }
-            }
-        }
-
-        for (int i = 0; i < input.length; i++) {
-            for (int j = 0; j < input[0].length; j++) {
-                if (input[i][j] == 'Y')
-                    input[i][j] = 'X';
-                System.out.print(Character.valueOf(input[i][j]));
-            }
-            System.out.println();
-        }
     }
 
     public static void main(String[] args) {
